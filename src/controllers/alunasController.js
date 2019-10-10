ba8b34760251a64bba17961ae8af8009bebba6b6
Larissayk <lari.yukie@gmail.com>
2019-10-09 22:23:10 -0300

    criação rota nascidasEmSp

--- src/controllers/alunasController.js
+++ src/controllers/alunasController.js
@@ -1,12 +1,45 @@
-const alunas = require('../model/alunas.json')
+const alunas = require("../model/alunas.json");
 
 exports.get = (req, res) => {
-    console.log(req.url)
-    res.status(200).send(alunas)
-}
+  console.log(req.url);
+  res.status(200).send(alunas);
+};
 
 exports.getById = (req, res) => {
-    const id = req.params.id
-    console.log(id)
-    res.status(200).send(alunas.find(aluna => aluna.id ==id))
+  const id = req.params.id;
+  if(id > 17 || id<= 0) {
+    res.redirect(301, "https://www.mercadolivre.com.br/")
+    // res.send('Não encontrei este ID!!')
+  }
+
+  console.log(id);
+  res.status(200).send(alunas.find(aluna => aluna.id == id));
+ 
+};
+
+exports.getBooks = (req, res) => {
+  const id = req.params.id;
+  const aluna = alunas.find(aluna => aluna.id == id)
+
+// if(!aluna) {
+//   res.send('Não encontrei esta garota!!')
+// }
+
+  const livrosAluna = aluna.livros
+  const livrosLidos = livrosAluna.filter(livro => livro.leu == "true")
+  const tituloLivros = livrosLidos.map(livro => livro.titulo)
+  res.status(200).send(tituloLivros);
+};
+
+exports.getSP = (req, res) => {
+  // const nasceuSp = alunas.nasceuEmSp
+  const paulistas = alunas.filter(item => { 
+    console.log(item)
+    return item.nasceuEmSp == "true" 
+  })
+      // vai trazer a array INTEIRA de todas as alunas que tenham a propriedade .nasceuEmSp == true
+  const nomePaulistas = paulistas.map(item => item.nome)     //vai mapear dentro da array de alunas nascidas em SP, somente a propriedade Nome
+  res.status(200). send(nomePaulistas);
 }
+
+
--- src/routes/alunasRoute.js
+++ src/routes/alunasRoute.js
@@ -3,6 +3,8 @@ const router = express.Router()
 const controller = require('../controllers/alunasController')
 
 router.get('/', controller.get)
+router.get('/nasceuSP', controller.getSP)
 router.get('/:id', controller.getById)
+router.get('/:id/books', controller.getBooks)
 
 module.exports = router
