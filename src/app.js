f69d03adac3cc85e59d2438c7c28fc100a62aed6
Larissayk <lari.yukie@gmail.com>
2019-10-21 19:15:33 -0300

    professoras controller

--- src/app.js
+++ src/app.js
@@ -1,6 +1,8 @@
 const express = require('express')
 const app = express()
 
+
+
 //rotas
 const index = require('./routes/index')
 const alunas = require('./routes/alunasRoute')
@@ -8,8 +10,9 @@ const professoras = require('./routes/professorasRoute')
 
 
 
-app.all('*', function(req, res, next) {
-    console.log("Passamos pelo app, uhuu!!")
+app.use(function(req, res, next) {
+    res.header('Access-Control-Allow-Origin','*')
+    res.header('Access-Control-Allow-Headers','Origin, X-Requested-With, Content-Type, Accept')
     next()
 })
 
--- src/controllers/professorasController.js
+++ src/controllers/professorasController.js
@@ -1,7 +1,44 @@
 const professoras = require("../model/professoras.json");
 
 exports.get = (req, res) => {
-  console.log(req.url);
-  
-  res.status(200).send(professoras);
+  const professoraSemCpf = professoras.map(item => {
+    item.cpf = "*********"
+    return item })
+
+  res.status(200).send({professoraSemCpf});
 };
+
+
+
+// const professoraSemCpf = professoras.map(item => {
+//   delete item.cpf
+//   return item 
+// })
+
+// exports.get = (req, res) => {
+//   const arrProfs = []
+//   for ()
+// }; 
+
+exports.getById = (req, res) => {
+  const id = req.params.id;
+  const professora = professoras.find(professora => professora.id == id)
+  const professoraSemCpf = professoras.map(item => {
+    delete item.cpf
+    return item 
+  })
+   
+  res.status(200).send(professora);
+    
+ 
+};
+
+// exports.getById = (req, res) => {
+//   const id = req.params.id;
+//   const professora = professoras.find(professora => professora.id == id)
+//       delete professora.cpf
+//   }
+   
+//   res.status(200).send(professora);
+    
+ 
